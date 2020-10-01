b2c07358280e2de6a186b76c2a103e49f8299027
Jaskaran Singh <jaskaran1523@gmail.com>
2020-10-01 05:51:09 -0700

    Add content

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './landing.scss';
+import logo from '../images/logo.png';
 
 const Landing = () => {
-	return <div id="landing"></div>;
+	return (
+		<div id="landing">
+			<div className="center-container">
+				<img src={logo} alt="Logo" className="logo-large" />
+				<p id="landing-text">
+					Find people in your location and date/study/create
+					professional networks or just make friends based on your
+					interests.
+				</p>
+				<button className="primary-button">Sign Up</button>
+				<p id="landing-signin-text">
+					Already have an account?
+					<a href="/" className="link">
+						{' '}
+						Login
+					</a>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Landing;
