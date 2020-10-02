b256469f29d2d57c2a40a68a81dc99053ed96553
Jaskaran Singh <jaskaran1523@gmail.com>
2020-10-01 22:05:21 -0700

    Add material core and icons dependencies

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Landing.scss';
 import logo from '../../images/logo.png';
+import EmailRoundedIcon from '@material-ui/icons/EmailRounded';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 const Landing = () => {
 	return (
@@ -12,7 +15,30 @@ const Landing = () => {
 					professional networks or just make friends based on your
 					interests.
 				</p>
-				<button className="primary-button">Sign Up</button>
+				<button className="primary-button">
+					<span className="signup-icon">
+						<EmailRoundedIcon />
+					</span>
+					Sign Up with Email
+				</button>
+				<button className="primary-button">
+					<span className="signup-icon">
+						<FacebookIcon />
+					</span>
+					Sign Up with Google
+				</button>
+				<button className="primary-button">
+					<span className="signup-icon">
+						<FacebookIcon />
+					</span>
+					Sign Up with Facebook
+				</button>
+				<button className="primary-button">
+					<span className="signup-icon">
+						<TwitterIcon />
+					</span>
+					Sign Up with Twitter
+				</button>
 				<p id="landing-signin-text">
 					Already have an account?
 					<a href="/" className="link">
